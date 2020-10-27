ee64138d50cef91434912d51a8ef326341b5874b
raj <royaji77@gmail.com>
2020-10-28 00:08:07 +0200

    Simple Express Server Up & Running

---
+++ backend/data/products.js
@@ -0,0 +1,101 @@
+const products = [
+  {
+    _id: '1',
+    name: 'Product #1',
+    image: '/images/prod1.jpg',
+    description:
+      'Euclid hundreds of thousands cosmos across the centuries cosmic fugue rich in heavy atoms. White dwarf vastness is bearable only through love muse about Tunguska event colonies vanquish the best',
+    brand: 'Larry Beckett',
+    category: 'Category 1',
+    price: 89.99,
+    countInStock: 3,
+    rating: 1,
+    numReviews: 2,
+  },
+
+  {
+    _id: '2',
+    name: 'Product #2',
+    image: '/images/prod2.jpg',
+    description:
+      'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+    //brand: 'Apple',
+    category: 'Category 2',
+    price: 89.99,
+    countInStock: 3,
+    rating: 2,
+    numReviews: 4,
+  },
+
+  {
+    _id: '3',
+    name: 'Product #3',
+    image: '/images/prod3.jpg',
+    description:
+      'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+    //brand: 'Apple',
+    category: 'Category 3',
+    price: 89.99,
+    countInStock: 3,
+    rating: 5,
+    numReviews: 3,
+  },
+
+  {
+    _id: '4',
+    name: 'Product #4',
+    image: '/images/prod4.jpg',
+    description:
+      'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+    //brand: 'Apple',
+    category: 'Category 4',
+    price: 89.99,
+    countInStock: 3,
+    rating: 0,
+    numReviews: 0,
+  },
+
+  {
+    _id: '5',
+    name: 'Product #5',
+    image: '/images/prod5.jpg',
+    description:
+      'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+    //brand: 'Apple',
+    category: 'Category 1',
+    price: 89.99,
+    countInStock: 3,
+    rating: 2.5,
+    numReviews: 3,
+  },
+
+  {
+    _id: '6',
+    name: 'Product #6',
+    image: '/images/prod6.jpg',
+    description:
+      'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+    //brand: 'Apple',
+    category: 'Category 2',
+    price: 89.99,
+    countInStock: 3,
+    rating: 5,
+    numReviews: 5,
+  },
+
+  {
+    _id: '7',
+    name: 'Product #7',
+    image: '/images/prod7.jpg',
+    description:
+      'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+    //brand: 'Apple',
+    category: 'Category 3',
+    price: 89.99,
+    countInStock: 3,
+    rating: 3.5,
+    numReviews: 1,
+  },
+]
+
+module.exports = products
